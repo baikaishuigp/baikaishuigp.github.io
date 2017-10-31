7c703b5e3c5020bab567b8c8f148c68427005a63
baikaishuigp <290538545@qq.com>
2017-11-01 00:27:03 +0800

    初次利用脚手架创建项目，设置主页面中分为上中下三部分，中间部分的高度动态计算出来。

--- README.md
+++
@@ -1,2 +0,0 @@
-# baikaishuigp.github.io
-学习vue-cli
---
+++ vue-cli-demo/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app" class="container-fluid">
+    <div class="row" v-bind:style="styleObject1">
+      <div class="col-md-12" >
+     <top-content></top-content>
+      </div>
+    </div>
+    <div class="row" v-bind:style="styleObject2">
+      <div class="col-md-12">
+        <router-view/>
+      </div>
+    </div>
+    <div class="row" v-bind:style="styleObject3">
+      <div class="col-md-12">
+     <bottom-content></bottom-content>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import BottomContent from './components/BottomContent/BottomContent.vue';
+import TopContent from './components/TopContent/TopContent.vue';
+
+export default{
+  name: 'app',
+  mounted(){
+    this.init();
+  },
+  components:{
+  "bottom-content":BottomContent,
+  "top-content":TopContent
+  },
+  data(){
+    return{
+      styleObject1:{
+        height:'100px',
+      backgroundColor:'#e2e2e2'
+      },
+      styleObject2:{
+        height:'0px'
+      },
+      styleObject3:{
+        height:"100px",
+        backgroundColor:'#e2e2e2'
+      }
+    }
+  },
+  methods:{
+    init(){
+      this.styleObject2.height=document.body.clientHeight- parseInt(this.styleObject1.height)-parseInt(this.styleObject3.height)+"px";
+    },
+    
+  }
+}
+</script>
